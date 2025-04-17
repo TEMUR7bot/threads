efa0914c9461a26ab3b1217be22d02454c9b7c64
TEMUR7bot <81215356+TEMUR7bot@users.noreply.github.com>
2025-04-17 17:03:52 +0500

    Update threads.py

--- threads.py
+++ threads.py
@@ -2,7 +2,7 @@ import requests
 import json
 from concurrent.futures import ThreadPoolExecutor
 
-# 10 ta URL ro'yxati (misol uchun dummy URL lar, o'rniga haqiqiy API lar qo'yish mumkin)
+
 urls = [
     "https://jsonplaceholder.typicode.com/posts/1",
     "https://jsonplaceholder.typicode.com/posts/2",
